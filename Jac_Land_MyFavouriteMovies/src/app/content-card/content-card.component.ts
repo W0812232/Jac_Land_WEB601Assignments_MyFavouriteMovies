a510809e5026592e47ef8911c94dcf406e28a8a2
W0812232 <122814270+W0812232@users.noreply.github.com>
2024-01-25 02:15:02 -0500

    import and export

--- Jac_Land_MyFavouriteMovies/src/app/content-card/content-card.component.ts
+++ Jac_Land_MyFavouriteMovies/src/app/content-card/content-card.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { ContentList } from '../helper-files/content-list';
+import { Content } from '../helper-files/content-interface';
 @Component({
   selector: 'app-content-card',
   standalone: true,
