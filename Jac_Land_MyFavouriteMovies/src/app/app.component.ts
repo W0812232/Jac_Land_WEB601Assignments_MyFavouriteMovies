47d4b2c4e176ad57d95d24bdf90ba68acac8ad30
W0812232 <122814270+W0812232@users.noreply.github.com>
2024-01-25 02:44:24 -0500

    Update app.component.ts

--- Jac_Land_MyFavouriteMovies/src/app/app.component.ts
+++ Jac_Land_MyFavouriteMovies/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ContentCardComponent } from "./content-card/content-card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, ContentCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
