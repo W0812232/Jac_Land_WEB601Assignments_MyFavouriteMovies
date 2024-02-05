bf72eb1aa5a6f09cd09d823a9f0974e2ab8276f3
W0812232 <122814270+W0812232@users.noreply.github.com>
2024-02-04 20:59:33 -0500

    titles

--- Jac_Land_MyFavouriteMovies/src/app/content-list/content-list.component.ts
+++ Jac_Land_MyFavouriteMovies/src/app/content-list/content-list.component.ts
@@ -8,31 +8,27 @@ import { Content } from '../helper-files/content-interface';
   templateUrl: './content-list.component.html',
   styleUrl: './content-list.component.scss'
 })
-export class ContentListComponent implements OnInit{
+export class ContentListComponent {
   // Define your array of content
-  name: string;
-  myContentArray: Content[];
-  movie1: Content = {
-    id: 1,
-    title: "Pirates of the Caribbean: At World's End",
-    description: 'Captain Barbossa, Will Turner and Elizabeth Swann must sail off the edge of the map, navigate treachery and betrayal, find Jack Sparrow, and make their final alliances for one last decisive battle.',
-    creator: 'Gore Verbinski',
-    imgURL: 'https://angular.io/assets/images/logos/angular/angular.png',
-    type: 'Adventure/Action'
-  };
-
-
-  movie2: Content = {
-    id: 2,
-    title: 'Blade',
-    description: 'A half-vampire, half-mortal man becomes a protector of the mortal race, while slaying evil vampires.',
-    creator: 'Stephen Norrington',
-    imgURL: 'https://angular.io/assets/images/logos/angular/angular.png',
-    type: 'Horror/Action'
-  };
-
-  ngOnInit(){
-    this.myContentArray = [this.movie1, this.movie2]
+  movies = [
+    {
+      id: 1,
+        title: "Pirates of the Caribbean: At World's End",
+        description: 'Captain Barbossa, Will Turner and Elizabeth Swann must sail off the edge of the map, navigate treachery and betrayal, find Jack Sparrow, and make their final alliances for one last decisive battle.',
+        creator: 'Gore Verbinski',
+        imgURL: '',
+        type: 'Adventure/Action'
+    },
+    {
+      id: 2,
+        title: 'Blade',
+        description: 'A half-vampire, half-mortal man becomes a protector of the mortal race, while slaying evil vampires.',
+        creator: 'Stephen Norrington',
+        imgURL: 'https://angular.io/assets/images/logos/angular/angular.png',
+        type: 'Horror/Action'
+    }
+  ]
   }
-}
+  
+ 
 /*'Jaws','Pirates of the Caribbean: At Worlds End', 'Blade', 'The Lord of the Rings: The Return of the King', 'A Fistful of Dollars', 'Dungeons & Dragons: Honor Among Thieves' */
