4f35922ca1195fede2a6302d09f4077b59377d1d
W0812232 <122814270+W0812232@users.noreply.github.com>
2024-02-05 05:23:00 -0500

    update

--- Jac_Land_MyFavouriteMovies/src/app/app.component.ts
+++ Jac_Land_MyFavouriteMovies/src/app/app.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 import { ContentCardComponent } from './content-card/content-card.component';
 import { ContentListComponent } from './content-list/content-list.component';
 
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, ContentCardComponent, ContentListComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+@NgModule({
+  declarations: [ContentListComponent],
+  imports: [BrowserModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
 })
 export class AppComponent {
   title = 'Jac_Land_MyFavouriteMovies';
