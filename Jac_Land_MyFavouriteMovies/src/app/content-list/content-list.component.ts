e00b3098fa563f9735ec7b50fd1450ff6f40c465
W0812232 <122814270+W0812232@users.noreply.github.com>
2024-02-11 19:43:27 -0500

    add pipe

--- Jac_Land_MyFavouriteMovies/src/app/content-list/content-list.component.ts
+++ Jac_Land_MyFavouriteMovies/src/app/content-list/content-list.component.ts
@@ -11,6 +11,7 @@ import { ContentCardComponent } from '../content-card/content-card.component';
 })
 export class ContentListComponent implements OnInit {
   // Define your array of content here so the variable is available in content-list.comp....html
+  name: string;
   myContentArray: Content[];
   contentItem = {
     id: 1,
@@ -41,7 +42,7 @@ export class ContentListComponent implements OnInit {
       "Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.",
     creator: 'Peter Jackson',
     imgURL: 'https://angular.io/assets/images/logos/angular/angular.png',
-    type: 'PG-13',
+    type: '',
     tags: ['Adventure','Fantasy']
   };
   contentItem4 = {
@@ -63,6 +64,26 @@ export class ContentListComponent implements OnInit {
     imgURL: 'https://angular.io/assets/images/logos/angular/angular.png',
     type: 'PG-13',
     tags: ['Action','Adventure','Comedy']
+  };
+  contentItem6 = {
+    id: 6,
+    title: 'Drive',
+    description:
+      "A mysterious Hollywood action film stuntman gets in trouble with gangsters when he tries to help his neighbor's husband rob a pawn shop while serving as his getaway driver.",
+    creator: 'Nicolas Winding Refn',
+    imgURL: 'https://angular.io/assets/images/logos/angular/angular.png',
+    type: 'R',
+    tags: ['Action','Drama']
+  };
+  contentItem7 = {
+    id: 7,
+    title: 'The Big Short',
+    description:
+      "In 2006-2007 a group of investors bet against the United States mortgage market. In their research, they discover how flawed and corrupt the market is.",
+    creator: 'Adam McKay',
+    imgURL: 'https://angular.io/assets/images/logos/angular/angular.png',
+    type: 'R',
+    tags: ['Biography','Comedy','Drama']
   };
 
   ngOnInit() {
