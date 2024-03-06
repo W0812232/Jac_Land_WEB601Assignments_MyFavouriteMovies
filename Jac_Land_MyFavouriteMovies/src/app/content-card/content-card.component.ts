9fd1e6ca4d11649dc4fb17842e0b4944eb6576fa
W0812232 <122814270+W0812232@users.noreply.github.com>
2024-03-06 02:58:16 -0500

    added directive

--- Jac_Land_MyFavouriteMovies/src/app/content-card/content-card.component.ts
+++ Jac_Land_MyFavouriteMovies/src/app/content-card/content-card.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Content } from '../helper-files/content-interface';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-content-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './content-card.component.html',
   styleUrl: './content-card.component.scss',
 })
