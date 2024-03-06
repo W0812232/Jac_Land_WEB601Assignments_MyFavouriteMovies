59bdcf1655a326c6dd1311359a20fec4ba6f73f1
W0812232 <122814270+W0812232@users.noreply.github.com>
2024-03-06 04:36:56 -0500

    Added directive to add under line and bold

--- Jac_Land_MyFavouriteMovies/src/app/hover-affect.directive.ts
+++ Jac_Land_MyFavouriteMovies/src/app/hover-affect.directive.ts
@@ -1,11 +1,28 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appHoverAffect]',
-  standalone: true
+  standalone: true,
 })
-export class HoverAffectDirective {
+export class HoverAffectDirective implements OnInit {
+  @Input () colour: string;
 
-  constructor() { }
-
+  constructor(private elm: ElementRef) {
+  }
+  ngOnInit(): void{ 
+    this.elm.nativeElement.style.textDecoration =
+    "none";
+  }
+  @HostListener('mouseover') onHover() {
+    if(this.colour == 'blue') {
+      this.elm.nativeElement.style.textDecoration = 'underline';
+  }
+    else {
+    this.elm.nativeElement.style.fontWeight = 'bold';
+    }
+  }
+    @HostListener('mouseleave') onUnHover() {
+      this.elm.nativeElement.style.textDecoration = 'none';
+      this.elm.nativeElement.style.fontWeight = 'normal';
+}
 }
